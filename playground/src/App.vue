fac3fce9d500092852e559a68533d2841f4a8efa
boichik <71084886+boichik@users.noreply.github.com>
2025-02-03 17:44:48 +0200

    use package in playground

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -5,10 +5,6 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import {
-  AppDateTimePicker,
-  AppTimePicker,
-} from '@boichikpro/vue3-date-time-picker';
 
 const model = ref(null);
 </script>
