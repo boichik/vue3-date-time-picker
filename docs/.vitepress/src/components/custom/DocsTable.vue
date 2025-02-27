cb4a5c06eea3369033b5461802c98e65f818b415
boichik <71084886+boichik@users.noreply.github.com>
2025-02-27 13:05:13 +0200

    fix badge in table doc

--- docs/.vitepress/src/components/custom/DocsTable.vue
+++ docs/.vitepress/src/components/custom/DocsTable.vue
@@ -20,17 +20,21 @@
         <td
           v-for="(col, colKey) in data.columns"
           :key="`${rowIndex}-${colKey}`"
-          class="docs-table__row-cell"
+          :class="getClassElement(row[colKey])"
         >
-          <div
-            class="docs-table__row-cell__content"
-            :class="getClassElement(row[colKey])"
-          >
+          <div class="docs-table__row-cell__content">
             <div
               v-html="
                 getSanitizedText(row[colKey].value, row[colKey].needTranslate)
               "
             ></div>
+
+            <Badge
+              v-if="row[colKey].version"
+              type="tip"
+              class="docs-table__row-cell__content__badge"
+              :text="row[colKey].version"
+            />
 
             <a v-if="row[colKey].link" :href="row[colKey].link?.href">
               {{ t(row[colKey].link?.text) }}
@@ -65,9 +69,17 @@ const { t } = useLocalization();
 const props = defineProps<{ data: DocsTableData }>();
 
 function getClassElement(el: DocsTableRowElement) {
-  return el.infoPopover || el.version
-    ? 'docs-table__row-cell__content--flex'
-    : undefined;
+  const classes = ['docs-table__row-cell'];
+
+  if (el.infoPopover) {
+    classes.push('docs-table__row-cell--flex');
+  }
+
+  if (el.version) {
+    classes.push('docs-table__row-cell--version');
+  }
+
+  return classes;
 }
 
 function getSanitizedText(value: unknown, needTranslate?: boolean) {
@@ -79,11 +91,25 @@ function getSanitizedText(value: unknown, needTranslate?: boolean) {
 
 <style lang="scss" scoped>
 .docs-table {
-  &__row-cell__content {
-    &--flex {
+  &__row-cell {
+    position: relative;
+
+    &--version {
+      padding: 30px 16px 8px;
+    }
+
+    &--flex &__content {
       display: inline-flex;
       align-items: center;
       justify-content: flex-start;
+    }
+
+    &__content {
+      &__badge {
+        position: absolute;
+        top: 9px;
+        right: 4px;
+      }
     }
   }
 }
