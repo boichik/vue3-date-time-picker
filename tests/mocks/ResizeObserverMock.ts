8dcba253f0212ea2705f9b941b03c5d17b2c4e23
boichik <naym.ov.lad@gmail.com>
2025-12-22 11:40:27 +0200

    refactor: enhance ResizeObserverMock with instance tracking and trigger functionality

--- tests/mocks/ResizeObserverMock.ts
+++ tests/mocks/ResizeObserverMock.ts
@@ -6,16 +6,48 @@ const { observe, disconnect, unobserve } = vi.hoisted(() => {
   };
 });
 
+const instances: ResizeObserverMock[] = [];
+
 class ResizeObserverMock {
   observe = observe;
   unobserve = unobserve;
   disconnect = disconnect;
   // eslint-disable-next-line no-undef
-  constructor(public callback: ResizeObserverCallback) {}
+  constructor(public callback: ResizeObserverCallback) {
+    instances.push(this);
+  }
 }
 
 function initResizeObserverMock() {
+  instances.length = 0;
   window.ResizeObserver = ResizeObserverMock;
 }
 
-export { observe, disconnect, unobserve, initResizeObserverMock };
+/**
+ * Function for manually calling the ResizeObserver callback.
+ * @param entries - array of entries (can pass partial data)
+ * @param index - instance index (default is the last created)
+ */
+function triggerResizeObserver(
+  entries: Partial<ResizeObserverEntry>[] = [],
+  index: number = instances.length - 1
+) {
+  const instance = instances[index];
+  if (instance) {
+    instance.callback(
+      entries as ResizeObserverEntry[],
+      instance as unknown as ResizeObserver
+    );
+  } else {
+    console.warn('No ResizeObserver instance found to trigger.');
+  }
+}
+
+export {
+  instances,
+  observe,
+  disconnect,
+  unobserve,
+  initResizeObserverMock,
+  triggerResizeObserver,
+};
