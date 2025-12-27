0b3745601af1de8ac4fa1175153365f86738a4ad
boichik <naym.ov.lad@gmail.com>
2025-12-27 12:13:55 +0200

    refactor: remove playground

--- playground/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
-import './style/index.scss';
-import '@boichikpro/vue3-date-time-picker/assets/style.css';
-import '@boichikpro/vue3-date-time-picker/assets/variables.css';
-createApp(App).mount('#app');
